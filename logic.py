f2b594743e9b896006b450925d1fdcea4a35a6ba
wiktor <wiktor.kycia@gmail.com>
2025-07-06 19:06:32 +0200

    create a method to calculate number of mines around

--- logic.py
+++ logic.py
@@ -6,20 +6,29 @@ class Board:
         self.width = width
         self.height = height
         self.num_mines = num_mines
+        self.board = np.zeros((self.height, self.width), dtype=int)
     
-    def create_board(self):
-        """Create a board with mines and numbers."""
-        # Initialize the board with zeros
-        board = np.zeros((self.height, self.width), dtype=int)
-        # Place mines randomly
+    def create_mines(self) -> None:
+        """Places mines on the board."""
         mine_positions = np.random.choice(self.width * self.height, self.num_mines, replace=False)
         print(mine_positions)
         for mine in mine_positions:
             x = mine % self.width
-            y = mine // self.height
-            board[y][x] = 1
-        print(board)
-                
+            y = mine // self.width
+            self.board[y][x] = 1
+        print(self.board)
+    
+    def count_mines_around(self, x, y) -> int:
+        result = 0
+        for i in range(max(y-1, 0), min(y+2, self.height)):
+            for j in range(max(x-1, 0), min(x+2, self.width)):
+                try:
+                    if self.board[i][j] == 1:
+                        result += 1
+                except IndexError:
+                    pass
+        return result                
 
-b = Board(10, 10, 20)
-b.create_board()
+b = Board(6, 4, 9)
+b.create_mines()
+print([[b.count_mines_around(i,j) for i in range(b.width)] for j in range(b.height)])
