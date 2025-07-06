b2e46ca43eeb9e5817e4157ad808cb74d50fce7e
wiktor <wiktor.kycia@gmail.com>
2025-07-06 17:58:33 +0200

    add a welcoming label and a button to start the game

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import tkinter as tk
 root = tk.Tk()
 root.geometry("450x400")
+root.title("minesweeper")
+
+label = tk.Label(root, text="Welcome to Minesweeper!")
+label.pack(pady=20)
+
+def start_game():
+    for widget in root.winfo_children():
+        widget.destroy()
+
+start_button = tk.Button(root, text="Start Game", command=start_game, padx=10, pady=5)
+start_button.pack()
 
 root.mainloop()
